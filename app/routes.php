77e4cdbce58e5e5371ee2956975bbba8324ddcfa
ipavl <ipavl@users.noreply.github.com>
2016-02-08 00:45:02 -0500

    Remove placeholder <title> text
    The <title> tag will also no longer display the delimiter if no title is
    sent to the view.

--- app/routes.php
+++ app/routes.php
@@ -3,5 +3,5 @@
 Flight::route('/', function() {
   $db = Flight::db();
   $projects = $db->query('SELECT * FROM projects ORDER BY date DESC;');
-  Flight::render_page('index', array('title' => 'hey', 'projects' => $projects));
+  Flight::render_page('index', array('projects' => $projects));
 });
